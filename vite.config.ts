9ef19486bb766c343106a17fda4fc525be3f521c
David <makaidavidlevente@gmail.com>
2026-09-21 11:38:17 +0200

    'save'

--- vite.config.ts
+++ vite.config.ts
@@ -22,8 +22,26 @@ export default defineConfig(({ mode }) => {
         stringArrayEncoding: ['base64'],
         stringArrayThreshold: 0.75,
         exclude: [/node_modules/]
-      } as any), // <--- Ez a kis "as any" azonnal elhallgattatja a TypeScript hibát!
+      } as any),
     ].filter(Boolean),
+    build: {
+      chunkSizeWarningLimit: 10000,
+      rollupOptions: {
+        output: {
+          // Ez a finomhangolt logika megakadályozza az ezer fájl létrejöttét
+          manualChunks(id) {
+            if (id.includes('node_modules')) {
+              // Ha Three.js-t vagy ehhez kapcsolódó 3D csomagot talál, azt egyetlen különálló "vendor-3d" fájlba gyűjti
+              if (id.includes('three') || id.includes('@react-three') || id.includes('fiber')) {
+                return 'vendor-3d';
+              }
+              // Minden más egyéb külső alapcsomagot (pl. React) egyetlen közös "vendor-core" fájlba rak
+              return 'vendor-core';
+            }
+          }
+        }
+      }
+    },
     resolve: {
       alias: {
         '@': path.resolve(import.meta.dirname, './src'),
